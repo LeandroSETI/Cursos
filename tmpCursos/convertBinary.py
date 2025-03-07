ff6bce0aa3dd4b296e4c005806e301da829f22aa
christiansanchez <christiansanchez@seti.com.co>
2025-03-07 13:35:56 -0500

    Commit para guardar

--- tmpCursos/convertBinary.py
+++ tmpCursos/convertBinary.py
@@ -1,10 +1,16 @@
+# def convertBinary(Num):
+#     if Num > 1:
+#         convertBinary(Num // 2)
+#     print(Num % 2, end='') 
+
+# # tomar el numero decimal del usuario
+# decimal = int(input("Ingrese un numero decimal: "))
+# convertBinary(decimal)
+
+#sin usar funciones recursivas
 def convertBinary(Num):
-    if Num > 1:
-        convertBinary(Num // 2)
-    print(Num % 2, end='')
-
-# tomar el numero decimal del usuario
-decimal = int(input("Ingrese un numero decimal: "))
-convertBinary(decimal)
-
-#imprimir el nuevo numero binario
+    binary = ""
+    while Num > 0:
+        binary = str(Num % 2) + binary
+        Num = Num // 2
+    return binary
