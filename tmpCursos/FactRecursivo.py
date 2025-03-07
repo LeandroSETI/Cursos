17cc85c7971475aae0c45362959ab174421b99cb
christiansanchez <christiansanchez@seti.com.co>
2025-03-07 16:43:56 -0500

    se genera el codigo recursivo y se revisa como funciona en prueba de escritorio

--- tmpCursos/FactRecursivo.py
+++ tmpCursos/FactRecursivo.py
@@ -9,7 +9,9 @@ def factorial(num):
     if num == 0 or num == 1:
         return 1
     else:
-        return num * factorial(num - 1)
+        return num * factorial(num - 1) 
+# el codigo llama a la definicion de la funcion factorial, es decir, se llama a si misma
+# en este caso se llama a si mismo desde 5 y va restando 1 hasta llegar a 1
     
 num = int(input("ingresar un número para calcular su factorial: "))
 print(f"El factorial de {num} es {factorial(num)}")
