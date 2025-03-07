bb04eddaa291c58672eaad5037260365f2d26f58
christiansanchez <christiansanchez@seti.com.co>
2025-03-07 09:40:14 -0500

    guardado del codigo

--- tmpCursos/contWord.py
+++ tmpCursos/contWord.py
@@ -13,17 +13,3 @@ def contWord(cadena):
 
 cadena = input("Ingresar cadena de caracteres: ")
 contWord(cadena)
-
-# import re  # Asegúrate de importar el módulo re
-
-# def contWord(cadena):
-#     # Buscar las palabras utilizando una expresión regular
-#     numPalabras = re.findall(r'\b\w+\b', cadena)
-#     # Contar las palabras
-#     conteo = len(numPalabras)
-#     # Imprimir el conteo de palabras
-#     print(f"El número de palabras en la cadena de caracteres es: {conteo}")
-
-# # Solicitar entrada al usuario
-# cadena = input("Ingresar cadena de caracteres: ")
-# contWord(cadena)
