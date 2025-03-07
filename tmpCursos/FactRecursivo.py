16fd4d6f3cfb5104079b29fa426ab536128fafe5
christiansanchez <christiansanchez@seti.com.co>
2025-03-07 16:28:21 -0500

    se encuentra el factorial con la funcion math

---
+++ tmpCursos/FactRecursivo.py
@@ -0,0 +1,11 @@
+import math
+
+
+# def factorial(num):
+#     if num == 0 or num == 1:
+#         return 1
+#     else:
+#         return num * factorial(num - 1)
+
+numero = int (int(input("ingresar un número para calcular su factorial: ")))
+print(f"El factorial de {numero} es {math.factorial(numero)}")
